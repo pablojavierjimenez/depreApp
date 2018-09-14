4c9480b367067b99d3863af782083b1c28b2a166
Javier Jimenez <pablojavierjimenez@gmail.com>
2018-09-14 10:45:37 -0300

    comenzando a revisar el desacoplado de archivos

---
+++ app/src/DomElements.class.js
@@ -0,0 +1,21 @@
+
+export class DomElements {
+/**
+ * @constructor
+ * @param {object} bom reference to a Browser object given by browser
+ * @param {objecta} dom reference to a document object given by browser
+ * @param {string}domSelector css id for modal container
+ * @example Example of implementation:
+ *      let modal = new ModalComponent(window, document, '.app_modal__container');
+ */
+  constructor() {
+    // Elements by Id
+    this.inputDate = document.getElementById('start_day');
+    this.inputSalary = document.getElementById('start_salary');
+    this.calculateBtn = document.getElementById('calculate');
+    this.salaryInitial = document.getElementById('salary_start_inDolars');
+    this.salaryActual = document.getElementById('salary_actual_inDolars');
+    this.salaryActualinPesos = document.getElementById('salary_actual_inPesos');
+    this.salaryActualinPesosWithOutDevaluation = document.getElementById('salary_actual_inPesos_without_devaluation');
+  }
+}
---
+++ app/src/operations.class.js
@@ -0,0 +1,7 @@
+
+export class Operations {
+  constructor() {
+
+  }
+
+}
